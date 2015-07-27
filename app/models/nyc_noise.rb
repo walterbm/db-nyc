5065d18c424e805e28c2667502a4ce4d01b32d4b
lizlove <elizabethlovero@gmail.com>
2015-07-27 15:48:20 -0400

    blerg

--- app/models/nyc_noise.rb
+++ app/models/nyc_noise.rb
@@ -1,2 +1,14 @@
 class NycNoise< ActiveRecord::Base
+  validates :latitude, :longitude, presence: true
+
+  def self.convert_to_float
+    self.latitude.to_f
+    self.longitude.to_f
+  end 
+
+  def self.convert_to_dt
+    self.created_date.gsub(/\/..$/, '2015')
+    self.created
+  end 
+
 end
