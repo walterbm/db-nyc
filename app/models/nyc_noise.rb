d11a2f5dfcfe79fa459d4e91c23bf8c526e19d5f
lizlove <elizabethlovero@gmail.com>
2015-07-28 08:54:06 -0400

    model updates

--- app/models/nyc_noise.rb
+++ app/models/nyc_noise.rb
@@ -1,17 +1,27 @@
 class NycNoise< ActiveRecord::Base
   validates :latitude, :longitude, presence: true
 
+  # def self.request(request_url)
+  #   base_url = ""
+  #   api_request = base_url + request_url
+  #   api_response = open(api_request).read
+  #   JSON.parse(api_response)["response"]
+  # end   
+
   def self.convert_to_float
     self.latitude.to_f
     self.longitude.to_f
   end 
 
-  # def hourly_breakout
-
+  # def self.hourly_breakout
+  # result = self.request("URL_HERE")
+  
   # end 
 
   # def noise_by_type
+  # result = self.request("URL_HERE")
 
   # end 
 
 end
+
