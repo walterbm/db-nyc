043a1dc6c01eccc4ad5e103485bd5110ad0c0934
walterbm <walter.beller.morales@gmail.com>
2015-07-29 16:39:28 -0400

    rework schema, seeds, and migrations

--- db/migrate/20150727174358_change_column_on_nyc_noises.rb
+++
@@ -1,6 +0,0 @@
-class ChangeColumnOnNycNoises < ActiveRecord::Migration
-  change_table :nyc_noises do |t|
-    t.change :created_date, :string
-    t.change :closed_date, :string
-  end
-end
--- db/migrate/20150729203821_create_nyc_noises.rb
+++ db/migrate/20150729203821_create_nyc_noises.rb
@@ -1,20 +1,14 @@
 class CreateNycNoises < ActiveRecord::Migration
   def change
     create_table :nyc_noises do |t|
-      t.integer :unique_key
       t.datetime :created_date
       t.datetime :closed_date
-      t.string :agency
       t.string :complaint_type
       t.string :descriptor
-      t.string :location_type
-      t.string :incident_zip
-      t.string :incident_address
       t.string :city
       t.string :borough
       t.decimal :latitude, {:precision=>10, :scale=>6}
       t.decimal :longitude, {:precision=>10, :scale=>6}
-      t.string :location
       t.timestamps null: false
     end
   end
