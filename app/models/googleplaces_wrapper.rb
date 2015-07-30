2f42f9365f5399bef3c929c0f602f56388734170
lizlove <elizabethlovero@gmail.com>
2015-07-30 11:23:36 -0400

    add googleplaceswrapper

--- app/models/googleplaces_wrapper.rb
+++ app/models/googleplaces_wrapper.rb
@@ -10,8 +10,13 @@ class GoogleplacesWrapper
   end
 
   def self.hospitals
-    
+    result = self.request("nearbysearch/json?" + "location=40.7731295,-73.957734" + "&radius=19000" + "&types=hospital" + "&key=" + GP_KEY)
+    hospital = Struct.new(:id, :lat, :lng)
+    hospitals = []
+    result["geometry"]["location"].each.with_index do |p,i|
+      hospitals << hospital.new(i, p["lat"], p["lng"])
+    end   
   end
 
-
 end
+
