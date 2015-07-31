b0d5270a27aaf9d3f8ac956e9bba496cbdad6010
lizlove <elizabethlovero@gmail.com>
2015-07-31 13:27:14 -0400

    button js

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -72,13 +72,9 @@ $(function() {
     }
   });
 
-   $(function(){
-
     $(".dropdown-menu").on('click', 'li a', function(){
       $(".btn:first-child").text($(this).text());
       $(".btn:first-child").val($(this).text());
    });
-
-  });
 
 });
