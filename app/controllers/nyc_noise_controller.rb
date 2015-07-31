353fabe8e052ec2587fcf44731472d46a47b644c
walterbm <walter.beller.morales@gmail.com>
2015-07-31 09:19:15 -0400

    controller cleanup

--- app/controllers/nyc_noise_controller.rb
+++ app/controllers/nyc_noise_controller.rb
@@ -1,23 +1,13 @@
 class NycNoiseController < ApplicationController
 
   def index
-    # @twenty_four_types = Aggregator.descriptors_twenty_four
-    # @all_layers = Aggregator.twenty_four_all
     @complaint_types = Aggregator.descriptors
-    @all_twenty_four = NycNoise.twenty_four
+    @all_twenty_four = Aggregator.twenty_four_all
   end
-
-  # def layer
-  #   render json: layer = Aggregator.twenty_four_layer(params[:layer])
-  # end
 
   def descriptor
     @descriptor = params[:descriptor]
     @twenty_four = Aggregator.twenty_four_layer(@descriptor)
-    #@twenty_four = NycNoise.twenty_four_by_description(@descriptor)
-    respond_to do |format|
-      format.js
-    end
   end
 
 end
